14b052d2c80875b9621704deb5b16a58e5424c5b
Gui12x9 <guilhermejesus568@gmail.com>
2024-03-10 14:21:37 -0300

    alteracoes html java css

--- script.js
+++ script.js
@@ -1,33 +1,31 @@
 // VARIÁVEIS => Um espaço da memória do computador que guardamos algo (um numero, uma letra, um texto, uma imagem)
 // FUNÇÃO => Um trecho de código que só é executado quando é chamado
 
-let chave = "cebcd482eda57fa9a6714c1c2ba91885"
+const key = "c0e9cf253ac7f2e3c94fe16a512c5470"
 
 
-function colocarNaTela(dados){
-    console.log(dados)
+function colocarDadosNaTela(dados){
+    
     document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name
-    document.querySelector(".temp").innerHTML =  Math.floor(dados.main.temp) + "°C"
-    document.querySelector(".descricao").innerHTML = dados.weather[0].description
-    document.querySelector(".icone").src = "https://openweathermap.org/img/wn/" + dados.weather[0].icon + ".png"
+document.querySelector(".temp").innerHTML = Math.floor(dados.main.temp) + "°C"
+document.querySelector(".descricao").innerHTML = dados.weather[0].description
+document.querySelector(".icone").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
+document.querySelector(".umidade").innerHTML = "Umidade: " + dados.main.humidity + "%"
 }
+
+
 
 async function buscarCidade(cidade){
-    let dados = await fetch("https://api.openweathermap.org/data/2.5/weather?q=" + 
-    cidade + 
-    "&appid=" + 
-    chave + 
-    "&lang=pt_br" +
-    "&units=metric"
-    )
-    .then(resposta => resposta.json())
 
-    colocarNaTela(dados)
+    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then(resposta => resposta.json())
+
+ colocarDadosNaTela(dados)
 }
 
 
-function cliqueiNoBotao(){
-   let cidade = document.querySelector(".input-cidade").value
 
-   buscarCidade(cidade)
+function cliqueiNoBotao() {   
+const cidade= document.querySelector(".input-cidade").value
+
+buscarCidade(cidade)
 }
